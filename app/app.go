db3d998f27f412813be4e8f9e84912678374f213
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-11-09 19:03:13 -0300

    criado função que busca os nameserver dos servidores

--- app/app.go
+++ app/app.go
@@ -13,18 +13,26 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPS e Nomes de Servidor na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "unidavi.edu.br",
+		},
+	}
+
 	//slice de comandos que aplicação vai executar
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "unidavi.edu.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -41,5 +49,18 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
