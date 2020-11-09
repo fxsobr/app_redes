c1c7864a637943ed8bdfcd4178c3a385ce2be40b
Marcelo Ceolin <mceolin@unidavi.edu.br>
2020-11-09 18:42:32 -0300

    inicializado app

--- app/app.go
+++ app/app.go
@@ -2,6 +2,11 @@ package app
 
 import "github.com/urfave/cli"
 
+// A função gerar vai retornar a aplicação de linha de comando, pronta para ser executada
 func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPS e Nomes de Servidor na Internet"
 
+	return app
 }
